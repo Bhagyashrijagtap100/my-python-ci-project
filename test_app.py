e52b2f965cce80068657a490c824722a36fabe2e
Bhagyashri <b8605787060>
2025-09-01 14:30:36 +0530

    feat: Add simple Python project and CI pipeline

---
+++ test_app.py
@@ -0,0 +1,10 @@
+from app import add
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+def test_add_negative_numbers():
+    assert add(-1, -5) == -6
+
+def test_add_zero():
+    assert add(0, 10) == 10
